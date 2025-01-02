093e9408e9521797fc5f2fe3ce78ab7f82b358af
OMAR-ABU-SNINEH <ooommmaaarrr548@gmail.com>
2025-01-02 20:24:33 +0300

    add TC001

--- src/tutorialsninja/register/TC_RF_001.java
+++ src/tutorialsninja/register/TC_RF_001.java
@@ -1,8 +1,11 @@
 package tutorialsninja.register;
 
 import java.time.Duration;
+import java.util.UUID;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -26,17 +29,51 @@ public class TC_RF_001 {
 
 	}
 
-	@Test
-	public void myTest() {
+	@Test(priority = 1, enabled = true)
+	public void register() {
 
+		WebElement myAccount = driver.findElement(By.xpath("//span[text()='My Account']"));
+		myAccount.click();
+
+		WebElement registerBtn = driver.findElement(By.linkText("Register"));
+		registerBtn.click();
+
+		WebElement firstNameField = driver.findElement(By.id("input-firstname"));
+		firstNameField.sendKeys("Omar");
+
+		WebElement lastNameField = driver.findElement(By.id("input-lastname"));
+		lastNameField.sendKeys("Abu Snineh");
+
+		String randomEmail = "omar_" + UUID.randomUUID().toString().substring(0, 6) + "@gmail.com"; // UUID ~
+																									// Universally
+																									// Unique Identifier
+		WebElement emailField = driver.findElement(By.id("input-email"));
+		emailField.sendKeys(randomEmail);
+
+		System.out.println("the email is " + randomEmail);
+
+		WebElement phoneField = driver.findElement(By.id("input-telephone"));
+		phoneField.sendKeys("078272800");
+
+		WebElement passwordField = driver.findElement(By.id("input-password"));
+		passwordField.sendKeys("12345678");
+
+		WebElement passwordConField = driver.findElement(By.id("input-confirm"));
+		passwordConField.sendKeys("12345678");
+
+		WebElement checkPolicy = driver.findElement(By.name("agree"));
+		checkPolicy.click();
+
+		WebElement continueBtn = driver.findElement(By.xpath("//input[@value='Continue']"));
+		continueBtn.click();
 	}
 
 	@AfterTest
 	public void cleanUp() {
 
-		if (driver != null) {
-			driver.quit();
-		}
+//		if (driver != null) {
+//			driver.quit();
+//		}
 
 	}
 }
